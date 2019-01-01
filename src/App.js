4b088d48dd02a451b3546053a6d73ad5b90554a6
ZeeshanTamboli <zeeshan.tamboli@gmail.com>
2019-01-01 16:30:59 +0530

    Pseudo selectiors in css modules

--- src/App.js
+++ src/App.js
@@ -45,16 +45,8 @@ class App extends Component {
   };
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
-
     let persons = null;
+    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
@@ -73,7 +65,7 @@ class App extends Component {
         </div>
       );
 
-      style.backgroundColor = 'red';
+      btnClass = styles.Red;
     }
 
     const classes = [];
@@ -88,7 +80,7 @@ class App extends Component {
       <div className={styles.App}>
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
-        <button style={style} onClick={this.togglePersonsHandler}>
+        <button className={btnClass} onClick={this.togglePersonsHandler}>
           Toggle Persons
         </button>
         {persons}
